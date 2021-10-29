3cafb36014d3e6a1ff389acde051a0282d3475b9
mnsh73 <manishme210@gmail.com>
2021-10-29 12:22:55 +0530

    second commit message

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass
 	public static void main(String[] args) {
 		
 		System.out.println("hello this is the first command");
+		System.out.println("this is second command");
 	}
 
 }
